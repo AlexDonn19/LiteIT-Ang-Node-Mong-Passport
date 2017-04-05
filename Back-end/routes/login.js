9f5dcea2d1f027689d7a070c10f2367f79fdc701
AlexDonn19 <danchukov_don@ukr.net>
2017-04-05 16:49:08 +0300

    login repair

--- Back-end/routes/login.js
+++ Back-end/routes/login.js
@@ -1,7 +1,13 @@
 ﻿var User = require('../models/user').User;
 var log = require('../libs/log')(module);
+var passport = require('passport');
+
+// token factory
+var oauth2 = require('../libs/oauth2');
+require('../libs/auth');
 
 exports.post = function(req, res) {
+
     return User.findOne( {username: req.body.username}, function (err, user) {
         if (err) {
             res.statusCode = 500;
@@ -10,6 +16,7 @@ exports.post = function(req, res) {
         };
         if (user) {
             if (user.checkPassword(req.body.password)) {
+                passport.authenticate('bearer', { session: false });
                 return res.send({ success: true, token: 1 });
             } else {
                 res.statusCode = 422;
